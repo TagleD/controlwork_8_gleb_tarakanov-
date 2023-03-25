b7af571e4631e78930b9b026b074720ea7e55018
GLeb Tarakanov <glebtgd@gmail.com>
2023-03-25 13:37:47 +0600

    Модель Product настроена в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from webapp.models import Product
+
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'image')
+    list_filter = ('id', 'name', 'category', 'description')
+    search_fields = ('id', 'name', 'category', 'description')
+    fields = ('id', 'name', 'category', 'description')
+    readonly_fields = ('id',)
+
+admin.site.register(Product, ProductAdmin)
